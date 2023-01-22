5ae110c358688cd23428fdbadff5625daad4f008
PiyushKhewalkar <khewalkarpiyush2005@gmail.com>
2023-01-22 19:00:00 +0530

    new

--- index.js
+++ index.js
@@ -26,3 +26,11 @@ const dropdownclicked = () => {
 
 dropdownbutton.addEventListener('click', dropdownclicked)
 
+// Grab HTML Elements
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// Add Event Listeners
+btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+});
