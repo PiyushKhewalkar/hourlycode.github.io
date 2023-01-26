f94c0694870629441cec8ee8167142dbe8bd58ce
PiyushKhewalkar <khewalkarpiyush2005@gmail.com>
2023-01-26 10:07:59 +0530

    latest

--- index.js
+++ index.js
@@ -17,21 +17,15 @@ button.addEventListener('click', () => {
 });
 
 
-const dropdownbutton = document.getElementById('drop-down')
-const level1 = document.getElementById('level1')
-
-const dropdownclicked = () => {
-    console.log('hi')
-}
-
-dropdownbutton.addEventListener('click', dropdownclicked)
 
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
 const toggleNavbar = () => {
+  // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
+
 
